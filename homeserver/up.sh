a5c95a03481f5542336a24d0143dad357d16cb8f
Emil Madsen <sovende@gmail.com>
2023-12-29 02:11:35 +0100

    Fix docker-compose command to docker compose

--- homeserver/up.sh
+++ homeserver/up.sh
@@ -4,5 +4,5 @@ COMPOSE_FILES="$(find . | grep "docker-compose.yaml")"
 
 for COMPOSE_FILE in $COMPOSE_FILES
 do
-    docker-compose -f "$COMPOSE_FILE" up -d --build
+    docker compose -f "$COMPOSE_FILE" up -d --build
 done
